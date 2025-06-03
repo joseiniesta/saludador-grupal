a6247c8e64dda0a7f3603942dd13415ace7b000d
joseiniesta <jose.iniesta@itgranada.com>
2025-06-04 01:15:27 +0200

    Update saludador.py

--- saludador.py
+++ saludador.py
@@ -1,1 +1,5 @@
 print("¡Bienvenidos al saludador grupal!")
+print("Hola, soy jose.")
+print("¡Espero que estés teniendo un buen día!")
+print("Hola, soy Jhonny walker.")
+print("¡Este es mi primer commit en grupo!")S
